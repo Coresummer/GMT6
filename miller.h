c6cd87a0df0d16b2e4c2362b75bc62cef101cec9
Coresummer <koresummer@gmail.com>
2021-01-25 18:03:14 +0900

    miller done

--- miller.h
+++ miller.h
@@ -3,20 +3,6 @@
 
 #include "create.h"
 
-// void efp6_to_efp(efp_t *ANS,efp6_t *A);
-// void efp_to_efp6(efp6_t *ANS,efp_t *A);
-// void efp6_to_efp3(efp3_t *ANS,efp6_t *A);
-// void efp3_to_efp6(efp6_t *ANS,efp3_t *A);
-// void ff_lttp_6sparse(fp6_t *f,efp3_t *T,efp_t *P);
-// void ff_lttp_7sparse(fp6_t *f,efp3_t *T,efp_t *P);
-// void f_ltqp_6sparse(fp6_t *f,efp3_t *T,efp3_t *Q,efp_t *P);
-// void f_ltqp_7sparse(fp6_t *f,efp3_t *T,efp3_t *Q,efp_t *P);
-// void Pseudo_7sparse_mapping(efp_t *P,efp3_t *Q);
-// void fp6_mul_6sparse(fp6_t *ANS,fp6_t *A,fp6_t *B_6sparse);
-// void fp6_mul_7sparse(fp6_t *ANS,fp6_t *A,fp6_t *B_7sparse);
-// void miller_ate_6sparse(fp6_t *f,efp6_t *P,efp6_t *Q);
-// void miller_ate_7sparse(fp6_t *f,efp6_t *P,efp6_t *Q);
-
 void efp6_to_Jacefp(efp_jacobian_t *ANS,efp6_t *A);
 void efp6_to_efp(efp_t *ANS,efp6_t *A);
 void ff_lttp(fp6_t *f, efp_jacobian_t *S, efp_t *P);
