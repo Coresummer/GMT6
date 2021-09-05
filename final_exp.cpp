8c56c21fef0b2c5f67e81cc5d1a0f15ab6a19bc4
Coresummer <koresummer@gmail.com>
2021-09-05 22:18:04 +0900

    final exp pow(128 reduced)

--- final_exp.cpp
+++ final_exp.cpp
@@ -135,29 +135,14 @@ void final_exp_lazy_montgomery2(fp6_t *ANS,fp6_t *A){
 
   fp6_frobenius_map_p1_montgomery(&tmp1_fp6,ANS);//(p^3-1)(p)
   fp6_mul_lazy_montgomery(ANS,ANS,&tmp1_fp6);         //(p^3-1)(p+1) = M
+  fp6_sqr_lazy_montgomery(&tmp12_fp6, ANS);
   //Second part
 //L1
-  fp6_frobenius_map_p3_montgomery(&tmp2_fp6,ANS);                     //M^-1                                       //I
-  fp6_sqr_GS_lazy_montgomery(&tmp3_fp6,&tmp2_fp6);               //M^-2                                       //S
-  //M^x                 //128
-
-  fp6_finalexpow_x_2NAF_lazy_montgomery(&tmp4_fp6, ANS);
-
-  fp6_mul_lazy_montgomery(&tmp5_fp6,&tmp4_fp6,&tmp3_fp6);     //M^(x-2) = -t0/x                            //M
-  fp6_frobenius_map_p3_montgomery(&tmp6_fp6,&tmp5_fp6);               //M^(-x+2) = t0/x                            //I
-
-  //M^(x^2-2x) = -t0                           //128
-  fp6_finalexpow_x_2NAF_lazy_montgomery(&tmp7_fp6, &tmp5_fp6);
-
-  fp6_mul_lazy_montgomery(&tmp8_fp6,&tmp6_fp6,&tmp7_fp6);     //M^(x^2-3x+2) = M^(-t0+t0/x) //reuse        //M
-  fp6_mul_lazy_montgomery(&tmp8_fp6,&tmp8_fp6,ANS);           //M^(x^2-3x+3) = M^(-t0+t0/x+1)              //M
-  fp6_sqr_GS_lazy_montgomery(&tmp9_fp6,&tmp8_fp6);
-  fp6_mul_lazy_montgomery(&tmp8_fp6,&tmp9_fp6,&tmp8_fp6);
-
-  fp6_frobenius_map_p1_montgomery(&tmp9_fp6,ANS);        //M^p                                        //p
-  fp6_frobenius_map_p3_montgomery(&tmp12_fp6,&tmp7_fp6);              //M^t0                                       //I
-  fp6_mul_lazy_montgomery(&tmp9_fp6,&tmp9_fp6,&tmp12_fp6);    //M^*(p+t0)                                  //M
-  fp6_mul_lazy_montgomery(&tmp9_fp6,&tmp9_fp6,&tmp3_fp6);     //M^*(p+t0-2) = M' right L1                  //M
+  fp6_frobenius_map_p3_montgomery(&tmp2_fp6,ANS);           //M^-1                                       //I
+  fp6_frobenius_map_p1_montgomery(&tmp3_fp6, ANS);          //M^p
+  fp6_finalexpow_x_2NAF_lazy_montgomery(&tmp4_fp6, ANS);    //M^x
+  fp6_mul_lazy_montgomery(&tmp9_fp6, &tmp2_fp6, &tmp3_fp6); // M^(p-1)
+  fp6_mul_lazy_montgomery(&tmp9_fp6, &tmp9_fp6, &tmp4_fp6); // M^(p-1+x) = M'
 
 //L2
   //M'^3w                                       //80
@@ -180,61 +165,9 @@ void final_exp_lazy_montgomery2(fp6_t *ANS,fp6_t *A){
   fp6_finalexpow_x_1_2NAF_lazy_montgomery(&tmp11_fp6, &tmp11_fp6);
 
   fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp7_fp6);  //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1) +(9w^2+3w) //M
- 
-  fp6_sqr_GS_lazy_montgomery(&tmp12_fp6,&tmp6_fp6);             //M'2*(3w+1) = M'^6w+2                          //S
-  fp6_mul_lazy_montgomery(&tmp12_fp6,&tmp12_fp6,&tmp9_fp6);  //M'^(6w+3)                                     //M
-  fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp12_fp6);  //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1) +(9w^2+3w) + (6w+3) //M
+  fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp5_fp6);  //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1) +(9w^2+6w) //M
+  fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp5_fp6);  //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1) +(9w^2+9w) //M
 
-  fp6_mul_lazy_montgomery(ANS,&tmp8_fp6,&tmp11_fp6);         //left * right = M^(3(-t0-x+3) + (p+t0-2)((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)+9w^2+9w + 3) //M
+  fp6_mul_lazy_montgomery(ANS,ANS,&tmp11_fp6);         //left * right = M^(1 + (p-1+x)((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)+9w^2+9w ) //M
+  fp6_mul_lazy_montgomery(ANS,ANS,&tmp12_fp6);         //left * right = M^(3 + (p-1+x)((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)+9w^2+9w ) //M
 }
-
-// void final_exp_lazy_montgomery2(fp6_t *ANS,fp6_t *A){
-//   static fp6_t tmp1_fp6, tmp2_fp6,tmp3_fp6,tmp4_fp6,tmp5_fp6,tmp6_fp6,tmp7_fp6,tmp8_fp6,tmp9_fp6,tmp10_fp6,tmp11_fp6,tmp12_fp6;
-
-//   fp6_inv_lazy_montgomery(&tmp1_fp6,A);
-//   fp6_frobenius_map_p3_montgomery(ANS,A);//(p^3)
-//   fp6_mul_lazy_montgomery(ANS,ANS,&tmp1_fp6);//(p^3-1)
-
-//   fp6_frobenius_map_p1_montgomery(&tmp1_fp6,ANS);//(p^3-1)(p)
-//   fp6_mul_lazy_montgomery(ANS,ANS,&tmp1_fp6);         //(p^3-1)(p+1) = M
-//   fp6_set(&tmp1_fp6, ANS);
-//   //Second part
-// //L1
-//   fp6_frobenius_map_p3_montgomery(&tmp6_fp6, ANS);            //M^-1
-//   fp6_sqr_GS_lazy_montgomery(&tmp6_fp6, &tmp6_fp6);           //M^-2
-//   fp6_frobenius_map_p1_montgomery(&tmp7_fp6, ANS);            //M^p
-//   fp6_finalexpow_x_2NAF_lazy_montgomery(&tmp8_fp6, ANS);      //M^x
-//   fp6_mul_lazy_montgomery(&tmp9_fp6,&tmp7_fp6,&tmp8_fp6);     //M^*(p+x-2)            
-//   // fp6_mul_lazy_montgomery(&tmp9_fp6, &tmp9_fp6, &tmp6_fp6);
-
-// //L2
-//   //M'^3w                                       //80
-//   fp6_finalexpow_3w_2NAF_lazy_montgomery(&tmp5_fp6, &tmp9_fp6);
-
-//   fp6_mul_lazy_montgomery(&tmp6_fp6,&tmp5_fp6,&tmp9_fp6);    //M'(3w+1)                                    //M
-  
-//   //M'^9w^2 +3w                                 //80
-//   fp6_finalexpow_3w_2NAF_lazy_montgomery(&tmp7_fp6, &tmp6_fp6);
-
-//   fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp7_fp6,&tmp9_fp6);   //M'^(9w^2+3w+1)                                //M
-
-//   //M'^(9w^2+3w+1)(x-1)                           //128
-//   fp6_finalexpow_x_1_2NAF_lazy_montgomery(&tmp11_fp6, &tmp11_fp6);
-
-//   fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp7_fp6);  //M'^(9w^2+3w+1)(x-1)+(9w^2+3w)                 //M
-//   fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp5_fp6);  //M'^(9w^2+3w+1)(x-1)+(9w^2+6w)                 //M
-
-//   //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)            //128
-//   fp6_finalexpow_x_1_2NAF_lazy_montgomery(&tmp11_fp6, &tmp11_fp6);
-
-//   fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp7_fp6);  //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1) +(9w^2+3w) //M
- 
-//   fp6_sqr_GS_lazy_montgomery(&tmp12_fp6,&tmp6_fp6);             //M'2*(3w+1) = M'^6w+2                          //S
-//   fp6_mul_lazy_montgomery(&tmp12_fp6,&tmp12_fp6,&tmp9_fp6);  //M'^(6w+3)                                     //M
-//   fp6_mul_lazy_montgomery(&tmp11_fp6,&tmp11_fp6,&tmp12_fp6);  //M'^((9w^2+3w+1)(x-1)+9w^2+6w)(x-1) +(9w^2+3w) + (6w+3) //M
-
-//   fp6_mul_lazy_montgomery(ANS, ANS, &tmp11_fp6);         //left * right = M^(1+ (p+t0-2)((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)+9w^2+9w + 3) //M
-//   fp6_mul_lazy_montgomery(ANS, ANS, &tmp1_fp6);          //left * right = M^(2+ (p+t0-2)((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)+9w^2+9w + 3) //M
-//   fp6_mul_lazy_montgomery(ANS, ANS, &tmp1_fp6);          //left * right = M^(3+ (p+t0-2)((9w^2+3w+1)(x-1)+9w^2+6w)(x-1)+9w^2+9w + 3) //M
-
-// }
