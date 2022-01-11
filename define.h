b5a291ddac3cb05bc5898d2a6711642ab01b02ff
Coresummer <koresummer@gmail.com>
2022-01-11 11:33:23 +0900

    update benchmark

--- define.h
+++ define.h
@@ -30,7 +30,7 @@
 
 // #define DEBUG_COST_A
 //#define DEBUG_ASSERT
-#define CHECK_PAIRING_TIME_LOOP 100000
+#define CHECK_PAIRING_TIME_LOOP 10000
 
 // **************Option**************/
 #define X64
