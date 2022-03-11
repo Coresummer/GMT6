cb28c87c03ab95b03eb562d524067bda8f5569b6
Coresummer <koresummer@gmail.com>
2022-03-11 12:08:39 +0900

    working on fp2_mul_base_nonmod_double

--- fp2.cpp
+++ fp2.cpp
@@ -788,9 +788,10 @@ void fp2_mul_base_nonmod_single(fp2_t *ANS,fp2_t *A){
 void fp2_mul_base_nonmod_double(fpd2_t *ANS,fpd2_t *A){
   //under construction
   fpd_t tmp1_fpd;
-  // const mp_limb_t *a = A->x0.x0;
-  // const mp_limb_t *b = A->x1.x0;
+  const mp_limb_t *a = A->x0.x0;
+  const mp_limb_t *b = A->x1.x0;
   // mcl_subDbl(&tmp1_fpd, (fpd_t*)prime672,&A->x1); //set neg
+  // mcl_negDbl(&tmp1_fpd.x0, &A->x1.x0);
   mcl_addDbl(ANS->x0.x0, A->x0.x0,A->x0.x0);
   mcl_addDbl(ANS->x1.x0, tmp1_fpd.x0,tmp1_fpd.x0);
 }
